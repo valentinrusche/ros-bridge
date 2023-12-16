bd6a4f291398f941ae3e0c32e10941dd66909396
Valentin Rusche <valentin.rusche@udo.edu>
2023-12-16 16:37:29 +0100

    fix syntax errors for the replay launch config

--- carla_replay_control/launch/carla_replay_control.launch.py
+++ carla_replay_control/launch/carla_replay_control.launch.py
@@ -4,6 +4,9 @@ import launch_ros.actions
 
 def generate_launch_description():
     ld = launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(name='host', default_value='localhost'),
+        launch.actions.DeclareLaunchArgument(name='port', default_value='2000'),
+        launch.actions.DeclareLaunchArgument(name='timeout', default_value='10'),
         launch.actions.DeclareLaunchArgument('log_file', default_value='latest.log'),
         launch.actions.DeclareLaunchArgument('start', default_value='0'),
         launch.actions.DeclareLaunchArgument('duration', default_value='300'),
@@ -19,17 +22,18 @@ def generate_launch_description():
         launch_ros.actions.Node(
             package='carla_replay_control',
             executable='carla_replay_control',
-            name=['carla_replay_control_', launch.substitutions.LaunchConfiguration('log_file')],
+            name='carla_replay_control',
             output='screen',
             emulate_tty=True,
             parameters=[
-                {
-                    'log_file': launch.substitutions.LaunchConfiguration('log_file'),
-                    'start': launch.substitutions.LaunchConfiguration('start'),
-                    'duration': launch.substitutions.LaunchConfiguration('duration'),
-                    'camera_id': launch.substitutions.LaunchConfiguration('camera_id'),
-                    'replay_sensors': launch.substitutions.LaunchConfiguration('replay_sensors')
-                }
+                {'host': launch.substitutions.LaunchConfiguration('host')},
+                {'port': launch.substitutions.LaunchConfiguration('port')},
+                {'timeout': launch.substitutions.LaunchConfiguration('timeout')},
+                {'log_file': launch.substitutions.LaunchConfiguration('log_file')},
+                {'start': launch.substitutions.LaunchConfiguration('start')},
+                {'duration': launch.substitutions.LaunchConfiguration('duration')},
+                {'camera_id': launch.substitutions.LaunchConfiguration('camera_id')},
+                {'replay_sensors': launch.substitutions.LaunchConfiguration('replay_sensors')}
             ]
         )
     ])
--- carla_replay_control/setup.py
+++ carla_replay_control/setup.py
@@ -22,9 +22,10 @@ elif ROS_VERSION == 2:
         version='0.0.1',
         packages=[package_name],
         data_files=[
-            ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+            ('share/ament_index/resource_index/packages',
+                ['resource/' + package_name]),
             ('share/' + package_name, ['package.xml']),
-            (os.path.join('share', package_name), glob('launch/*.launch.py'))
+            (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         ],
         install_requires=['setuptools'],
         zip_safe=True,
