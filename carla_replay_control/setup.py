860c1ccb30e9a71acca53009bd43354e2dc89493
Valentin Rusche <valentin.rusche@udo.edu>
2023-12-16 17:18:49 +0100

    add correct launch path

--- carla_replay_control/setup.py
+++ carla_replay_control/setup.py
@@ -24,7 +24,7 @@ elif ROS_VERSION == 2:
         data_files=[
             ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
             ('share/' + package_name, ['package.xml']),
-            (os.path.join('share', package_name), glob('launch/*.launch.py'))
+            (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
         ],
         install_requires=['setuptools'],
         zip_safe=True,
